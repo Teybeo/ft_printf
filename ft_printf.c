af11cc5c4eb70f2b8fb1c4d234712e64f49c8fd4
Thibault D'ARCHIVIO <tdarchiv@student.42.fr>
2018-10-29 13:41:19 +0100

    Remove old wip

--- ft_printf.c
+++ ft_printf.c
@@ -28,7 +28,7 @@ struct s_arg
 {
 	bool alternate_form;
 	bool left_adjust; // Default to right adjust
-	bool zero_padding; // Default to blank-padding
+	bool pad_with_zero; // Default to blank-padding
 	bool positive_sign;
 	bool space_positive;
 	bool long_modifier;
@@ -99,7 +99,7 @@ t_arg get_next_arg(const char *string, int *consumed)
 		}
 		else if (*ptr == '0')
 		{
-			arg.zero_padding = true;
+			arg.pad_with_zero = true;
 			while (*ptr == '0')
 				ptr++;
 			ptr--;
@@ -173,7 +173,6 @@ void process_arg(t_array *output, t_arg arg, va_list list)
 		}
 		else
 		{
-#if 1
 			size_t itoa_len;
 			int d = va_arg(list, int);
 
@@ -183,15 +182,15 @@ void process_arg(t_array *output, t_arg arg, va_list list)
 			int is_neg = d < 0;
 			int has_sign_char = ft_isdigit(new[0]) == false;
 			size_t digit_count = itoa_len - has_sign_char;
-			if (arg.min_width > 0 && arg.zero_padding && arg.has_precision == false)
+			if (arg.min_width > 0 && arg.pad_with_zero && arg.has_precision == false)
 			{
 				arg.precision = arg.min_width - has_sign_char;
 				arg.min_width = 0;
-				arg.zero_padding = false;
+				arg.pad_with_zero = false;
 			}
 			// Precision forces padding with ' ' instead of '0'
-			if (arg.has_precision && arg.zero_padding)
-				arg.zero_padding = false;
+			if (arg.has_precision && arg.pad_with_zero)
+				arg.pad_with_zero = false;
 			ft_memcpy(new, new + has_sign_char, digit_count); // Remove sign in string
 
 			size_t core_length = ft_max(arg.precision, digit_count) + has_sign_char;
@@ -206,23 +205,8 @@ void process_arg(t_array *output, t_arg arg, va_list list)
 				temp[sign_idx] = (is_neg) ? '-' : arg.plus_sign;
 			}
 			ft_memcpy(temp + (final_length - digit_count), new, digit_count);
-			ft_memset(temp, arg.zero_padding ? '0' : ' ', final_length - core_length);
+			ft_memset(temp, arg.pad_with_zero ? '0' : ' ', final_length - core_length);
 			array_append(output, temp, final_length);
-#else
-			int d = va_arg(list, int);
-			if (arg.space_positive && d >= 0)
-				array_append(output, " ", 1);
-			if (arg.positive_sign && d >= 0)
-				array_append(output, "+", 1);
-			new = ft_itoa(d);
-			if (new[0] == '-')
-				array_append(output, "-", 1);
-			conversion_length = (size_t)ft_max(ft_strlen(new), arg.min_width);
-			if (arg.precision > conversion_length)
-				append_n_chars(output, '0', arg.precision - conversion_length);
-			array_append(output, new, ft_strlen(new));
-			free(new);
-#endif
 		}
 	}
 }
